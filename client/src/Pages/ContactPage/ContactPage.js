f47901fe250c41b079ac33b96797d5469615d3e0
Jose Ramirez <josemramirez81@outlook.com>
2021-11-23 07:57:33 -0600

    Create contact form

--- client/src/Pages/ContactPage/ContactPage.js
+++ client/src/Pages/ContactPage/ContactPage.js
@@ -1,3 +1,31 @@
 import './ContactPage.scss'
+import { Page } from '../../components/Page/Page'
 
-export const ContactPage = () => <h1>Contact</h1>
+export const ContactPage = () => {
+    return(
+        <Page>
+            <section className="contact">
+                <h1 className="contact__title">Contact Me</h1>
+                <form className="contact__form" asp-page="'./Contact" method="post" enctype="multipart/form-data">
+                    <div className="contact__form__controls">
+                        <label className="contact__form__controls__label">Name</label>
+                        <input className="contact__form__controls_input" type="text" asp-for="Email.Name"/>
+                    </div>
+                    <div className="contact__form__controls">
+                        <label className="contact__form__controls__label">Email</label>
+                        <input className="contact__form__controls_input" type="text" asp-for="Email.Email"/>
+                    </div>
+                    <div className="contact__form__controls">
+                        <label className="contact__form__controls__label">Subject</label>
+                        <input className="contact__form__controls_input" type="text"  asp-for="Email.Subject"/>
+                    </div>
+                    <div className="contact__form__controls">
+                        <label className="contact__form__controls__label">Message</label>
+                        <textarea className="contact__form__controls_input contact__form__controls_input--text"  asp-for="Email.Message"></textarea>
+                    </div>
+                    <input type="submit" className="contact__form__controls__submit" value="Submit"/>
+                </form>
+            </section>
+        </Page>
+    )
+}
