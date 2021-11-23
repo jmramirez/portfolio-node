450b2ec7083067ba2d77a1bbd603b1b9f1f0aab9
Jose Ramirez <josemramirez81@outlook.com>
2021-11-22 19:02:46 -0600

    Add content to HomePage

--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -1,3 +1,23 @@
 import './HomePage.scss'
+import { Page } from '../../components/Page/Page'
+import profile from '../../assets/img/jose_img_2.jpg'
 
-export const HomePage = () => <h1>Home Page</h1>
+export const HomePage = () => (
+    <Page>
+        <section className="profile">
+            <div className="profile__photo">
+                <img src={profile} alt="jose_img" className="profile__photo__image" />
+            </div>
+            <div className="profile__info">
+                <h1 className="profile__info__heading">Jose <span>Ramirez</span></h1>
+                <h3 className="profile__info__sub-heading">Full Stack Developer</h3>
+                <p className="profile__info__paragraph">I'm a full stack developer with experience on node Javascript .NET, C# and much more.</p>
+            <div className="profile__info__action">
+                <a href="https://github.com/jmramirez" target="_blank" rel="noreferrer" className="profile__info__action-button profile__info__action-button--social"><i className="fab fa-github main-info__social__icon"></i></a>
+                <a href="https://www.linkedin.com/in/jmramirez31/" target="_blank" rel="noreferrer" className="profile__info__action-button profile__info__action-button--social"><i className="fab fa-linkedin main-info__social__icon"></i></a>
+            <button className="profile__info__action-button profile__info__action-button--cta">Contact Me</button>
+        </div>
+    </div>
+</section>
+    </Page>
+)
